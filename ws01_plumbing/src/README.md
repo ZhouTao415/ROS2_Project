542364f3738ee655b6d9d4cfe0a94b361b29a072
ZhouTao415 <dylanzhou415@gmail.com>
2023-07-27 17:53:11 +0200

    update the file

--- ws01_plumbing/src/README.md
+++ ws01_plumbing/src/README.md
@@ -1,6 +1,6 @@
 # ROS2_Project
 
-## 话题通信之原生消息
+## 1. 话题通信之原生消息
 
 ### Publisher: 话题发布 
 ```bash
@@ -9,4 +9,16 @@ ros2 run cpp01_topic demo01_talker_str
 ### Subscription: 话题订阅
 ```bash
 ros2 run cpp01_topic demo02_listener_str 
+```
+## 2. 话题通信之自定义接口消息
+
+### Publisher: 话题发布 
+```bash
+ros2 run cpp01_topic demo03_talker_stu
+```
+
+### 检验话题是否发布出去 
+    "chatter_stu" is topic name 
+```bash
+ros2 topic echo /chatter_stu
 ```
--- ws01_plumbing/src/cpp01_topic/src/demo03_talker_stu.cpp
+++ ws01_plumbing/src/cpp01_topic/src/demo03_talker_stu.cpp
@@ -1,52 +1,102 @@
 /*  
-  需求：以某个固定频率发送文本学生信息，包含学生的姓名、年龄、身高等数据。
+ 需求：以某个固定频率发送文本学生信息，包含学生的姓名、年龄、身高等数据。
+
+ 步骤：
+   1.包含头文件；
+   2.初始化 ROS2 客户端；
+   3.定义节点类；
+     3-1.创建发布方；
+     3-2.创建定时器；控制发布频率
+     3-3.组织消息并发布。
+   4.调用spin函数，并传入节点对象指针；
+   5.释放资源.
+
+
 */
 
-// 1.包含头文件；
 #include "rclcpp/rclcpp.hpp"
 #include "base_interfaces_demo/msg/student.hpp"
 
-using namespace std::chrono_literals;
 using base_interfaces_demo::msg::Student;
+using namespace std::chrono_literals;
 
+class TalkerStu :public rclcpp::Node{
+public:
+  TalkerStu() : Node("talkerstu_node_cpp"), age(0){
+    publischer_ = this->create_publisher<Student>("chatter_stu", 10);
+    timer_ = this->create_wall_timer(500ms, std::bind(&TalkerStu::on_timer, this));
+  }
 
-// 3.定义节点类；
-class MinimalPublisher : public rclcpp::Node
-{
-  public:
-    MinimalPublisher()
-    : Node("student_publisher"), count_(0)
-    {
-      // 3-1.创建发布方；
-      publisher_ = this->create_publisher<Student>("topic_stu", 10);
-      // 3-2.创建定时器；
-      timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
-    }
-
-  private:
-    void timer_callback()
-    {
-      // 3-3.组织消息并发布。
-      auto stu = Student();
-      stu.name = "张三";
-      stu.age = count_++;
-      stu.height = 1.65;
-      RCLCPP_INFO(this->get_logger(), "学生信息:name=%s,age=%d,height=%.2f", stu.name.c_str(),stu.age,stu.height);
-      publisher_->publish(stu);
-
-    }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<Student>::SharedPtr publisher_;
-    size_t count_;
+private:
+  void on_timer(){
+    auto stu = Student();
+    stu.name = "Xin";
+    stu.age = age;
+    stu.height = 1.70;
+    age++;
+    publischer_->publish(stu);
+    RCLCPP_INFO(this->get_logger(), "发布的消息：(%s, %d, %2f)", stu.name.c_str(), stu.age, stu.height);
+    
+  }
+  rclcpp::Publisher<Student>::SharedPtr publischer_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  int age;
 };
 
-int main(int argc, char * argv[])
-{
-  // 2.初始化 ROS2 客户端；
+
+int main(int argc, char const* argv[]){
   rclcpp::init(argc, argv);
-  // 4.调用spin函数，并传入节点对象指针。
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
-  // 5.释放资源；
+  rclcpp::spin(std::make_shared<TalkerStu>());
   rclcpp::shutdown();
+  
   return 0;
 }
+
+// // 1.包含头文件；
+// #include "rclcpp/rclcpp.hpp"
+// #include "base_interfaces_demo/msg/student.hpp"
+
+// using namespace std::chrono_literals;
+// using base_interfaces_demo::msg::Student;
+
+
+// // 3.定义节点类；
+// class MinimalPublisher : public rclcpp::Node
+// {
+//   public:
+//     MinimalPublisher()
+//     : Node("student_publisher"), count_(0)
+//     {
+//       // 3-1.创建发布方；在下面创建返回值，并接收这个publischer对象指针
+//       publisher_ = this->create_publisher<Student>("topic_stu", 10);
+//       // 3-2.创建定时器；
+//       timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
+//     }
+
+//   private:
+//     void timer_callback()
+//     {
+//       // 3-3.组织消息并发布。
+//       auto stu = Student();
+//       stu.name = "张三";
+//       stu.age = count_++;
+//       stu.height = 1.65;
+//       RCLCPP_INFO(this->get_logger(), "学生信息:name=%s,age=%d,height=%.2f", stu.name.c_str(),stu.age,stu.height);
+//       publisher_->publish(stu);
+
+//     }
+//     rclcpp::TimerBase::SharedPtr timer_;
+//     rclcpp::Publisher<Student>::SharedPtr publisher_;
+//     size_t count_;
+// };
+
+// int main(int argc, char * argv[])
+// {
+//   // 2.初始化 ROS2 客户端；
+//   rclcpp::init(argc, argv);
+//   // 4.调用spin函数，并传入节点对象指针。
+//   rclcpp::spin(std::make_shared<MinimalPublisher>());
+//   // 5.释放资源；
+//   rclcpp::shutdown();
+//   return 0;
+// }

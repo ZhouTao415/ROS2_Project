e4df19f907e503f32c71992e0e9694beea1aedc1
ZhouTao415 <dylanzhou415@gmail.com>
2023-08-13 17:37:12 +0200

    update the action server

--- ws01_plumbing/src/cpp03_action/src/demo01_action_server.cpp
+++ ws01_plumbing/src/cpp03_action/src/demo01_action_server.cpp
@@ -70,8 +70,21 @@ public:
     using GoalCallback = std::function<GoalResponse(
         const GoalUUID &, std::shared_ptr<const typename ActionT::Goal>)>;
     */
-    rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const Progress::Goal>){
+    rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const Progress::Goal> goal){
+      // uuid 与 终端出现的goal accepted with ID 是呼应的，为客户端生成一个独一无二的标识
+      // 现在没用
+      (void)uuid;
+      
+
+      // goal 可以拿到目标值，是个指针
       // 关于目标处理的，一般是接收或者拒绝
+      // 业务逻辑： 判断提交的数字是否大于1,是就接收，否则就拒绝
+      if(goal->num <= 1) {
+        RCLCPP_INFO(this->get_logger(), "提交的目标之必须大于1!");
+        return rclcpp_action::GoalResponse::REJECT;
+      }
+
+      RCLCPP_INFO(this->get_logger(),"提交的目标值合法!");
       return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     }
 
@@ -81,8 +94,9 @@ public:
     /*     
     using CancelCallback = std::function<CancelResponse(std::shared_ptr<ServerGoalHandle<ActionT>>)>;
     */
-    rclcpp_action::CancelResponse handle_cancel(std::shared_ptr<rclcpp_action::ServerGoalHandle<Progress>> go_handler){
-      
+    rclcpp_action::CancelResponse handle_cancel(std::shared_ptr<rclcpp_action::ServerGoalHandle<Progress>> goal_handler){
+      (void) goal_handler;
+      RCLCPP_INFO(this->get_logger(), "接收的任务被取消！");
       return rclcpp_action::CancelResponse::ACCEPT;
     }
 
@@ -92,9 +106,78 @@ public:
     /*     
     using AcceptedCallback = std::function<void (std::shared_ptr<ServerGoalHandle<ActionT>>)>;
     */
-   void handle_accept(std::shared_ptr<rclcpp_action::ServerGoalHandle<Progress>>go_handler){
-    
-   }
+    // 这个过程是个耗时操作，为了避免主逻辑出现堵塞，单独开一个线程，来处理连续反馈和响应
+    void execute(std::shared_ptr<rclcpp_action::ServerGoalHandle<Progress>> goal_handler){
+
+      // 1. 需要生成连续反反馈， 返回给客户端
+      // void publish_feedback(std::shared_ptr<base_interfaces_demo::action::Progress_Feedback> feedback_msg)
+      // goal_handler->publish_feedback();
+      // 首先获取目标值，然后遍历，遍历中进行累加，且每循环一次就计算进度，并作为连续反馈发布
+
+      int num = goal_handler->get_goal()->num;
+      int sum = 0;
+      auto feedback = std::make_shared<Progress::Feedback>();
+      // 设置休眠 1.0 == 1HZ
+      rclcpp::Rate rate(1.0);
+      auto result = std::make_shared<Progress::Result>();
+
+      for (int i = 1; i <= num; i++)
+      {
+        sum += i;
+
+        // 累积过程中计算一个进度
+        // 计算生成一个进度，设计进入Feedback
+        double progess = i / (double)num;
+
+        // 设计进入Feedback
+        feedback->progress = progess;
+
+        // 发布操作
+        goal_handler->publish_feedback(feedback);
+        
+        RCLCPP_INFO(this->get_logger(), "连续反馈中，进度： %.2f", progess);
+
+        // 解决取消任务时候的bug
+        // 判断是否接收到了取消请求
+        // goal_handler->is_canceling()
+        // void canceled(std::shared_ptr<base_interfaces_demo::action::Progress_Result> result_msg)
+        // goal_handler->canceled();
+        if (goal_handler->is_canceling()) {
+          
+          // 如果接收到了就终止程序，return
+          result->sum = sum;
+          goal_handler->canceled(result);
+          RCLCPP_INFO(this->get_logger(), "任务被取消!");
+          return;
+        }
+
+        
+
+        // 休眠一秒钟，为了显示是个耗时操作
+        rate.sleep();
+      }
+      
+
+      // 2. 生成最终响应结果
+      // void succeed(std::shared_ptr<base_interfaces_demo::action::Progress_Result> result_msg)
+      // goal_handler->succeed();
+
+      if(rclcpp::ok()) {
+        // 检验服务端是否正常运行
+        result->sum = sum;
+        // 把我们最终结果响应回去
+        goal_handler->succeed(result);
+
+        RCLCPP_INFO(this->get_logger(), "最终结果： %d", sum);
+      }
+
+    }
+
+    void handle_accept(std::shared_ptr<rclcpp_action::ServerGoalHandle<Progress>> goal_handler){
+      // 新建线程处理耗时主逻辑实现
+      // 匿名线程,执行体,this 调用，传入参数 goal_handler
+      std::thread(std::bind(&ProgressActionServer::execute, this, goal_handler)).detach();
+    }
 
 
 
